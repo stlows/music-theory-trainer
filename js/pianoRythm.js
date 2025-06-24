12165713b9c0169e907a47c734851a579df64c04
stlows <vincent_beaulieu@live.ca>
2025-06-23 23:27:04 -0400

    beat on staff

--- js/pianoRythm.js
+++ js/pianoRythm.js
@@ -1,122 +1,163 @@
 const possibleTiming = [
-  { name: "Quarter", notation: "G4", text: "#" },
-  { name: "2-Eights", notation: "G2G2", text: "# &" },
-  { name: "4-Sixteenths", notation: "GGGG", text: "# e & a" },
-  { name: "Eight 2-Sixteenths", notation: "G2GG", text: "# & a" },
-  { name: "Sixteenth-Eight-Sixteenth", notation: "GG2G", text: "# e a" },
-  { name: "2-sixteenths Eight", notation: "GGG2", text: "# e &" },
-  { name: "Triplet", notation: "(3G2G2G2", text: "# tri plet" },
-  { name: "Dotted-Eight Sixteenth", notation: "G3G", text: "# a" },
-  { name: "Sixteenth Dotted-Eight", notation: "GG3", text: "# e" },
-  { name: "Rest Eight", notation: "z2G2", text: "&" },
-  { name: "Rest 2-Sixteenths", notation: "z2GG", text: "& a" },
-  { name: "Rest 3-Sixteenths", notation: "zGGG", text: "e & a" },
-  { name: "Rest Sixteenth Eight", notation: "zGG2", text: "e &" },
-  { name: "Rest Sixteenth", notation: "z3G", text: "a" },
-  { name: "Rest Dotted Eight", notation: "zG3", text: "e" },
-  { name: "Rest Eight-Sixteenth", notation: "zG2G", text: "e a" },
-  { name: "Rest Triplet", notation: "(3z2G2G2", text: "tri plet" },
-]
+  { name: "Quarter", notation: "B4", text: "#" },
+  { name: "2-Eights", notation: "B2B2", text: "# &" },
+  { name: "4-Sixteenths", notation: "BBBB", text: "# e & a" },
+  { name: "Eight 2-Sixteenths", notation: "B2BB", text: "# & a" },
+  { name: "Sixteenth-Eight-Sixteenth", notation: "BB2B", text: "# e a" },
+  { name: "2-sixteenths Eight", notation: "BBB2", text: "# e &" },
+  { name: "Triplet", notation: "(3B2B2B2", text: "# tri plet" },
+  { name: "Dotted-Eight Sixteenth", notation: "B3B", text: "# a" },
+  { name: "Sixteenth Dotted-Eight", notation: "BB3", text: "# e" },
+  { name: "Rest Eight", notation: "z2B2", text: "&" },
+  { name: "Rest 2-Sixteenths", notation: "z2BB", text: "& a" },
+  { name: "Rest 3-Sixteenths", notation: "zBBB", text: "e & a" },
+  { name: "Rest Sixteenth Eight", notation: "zBB2", text: "e &" },
+  { name: "Rest Sixteenth", notation: "z3B", text: "a" },
+  { name: "Rest Dotted Eight", notation: "zB3", text: "e" },
+  { name: "Rest Eight-Sixteenth", notation: "zB2B", text: "e a" },
+  { name: "Rest Triplet", notation: "(3z2B2B2", text: "tri plet" },
+];
 
 function pianoRythm() {
-  let el = div()
-  let staffDiv = div()
-  let staffwidth = document.getElementById("game").scrollWidth - 50
-  let scale = window.innerWidth > 700 ? 1.5 : 1
-  let measures = []
+  let el = div();
+  let staffDiv = div();
+  let staffwidth = document.getElementById("game").scrollWidth - 50;
+  let scale = window.innerWidth > 700 ? 1.5 : 1;
+  let measures = [];
   for (let measure = 0; measure < 1; measure++) {
-    let notes = []
+    let notes = [];
     for (let beat = 0; beat < 4; beat++) {
-      notes.push(chooseOne(possibleTiming.filter(x => settings.pianoRythm.includes(x.name))))
+      notes.push(chooseOne(possibleTiming.filter((x) => settings.pianoRythm.includes(x.name))));
     }
-    measures.push(notes)
+    measures.push(notes);
   }
   let abcString = `
 X: 1
 M: 4/4
 L: 1/16
-K: C
-|:${measures.map(m => m.map(x => x.notation).join(" ")).join("|")}:|
-w:${measures.map(m => m.map((x, index) => x.text.replace("#", index + 1)).join(" ")).join("|")}
-`
-  let staff = ABCJS.renderAbc(staffDiv, abcString, { scale, staffwidth, paddingLeft: 0, paddingRight: 0 })
-  let beats = []
-  let tripletMultiplier = 1
-  let count = 0
-  let beat = []
-
-  for (let note of staff[0].lines[0].staff[0].voices[0].filter(x => x.type !== "bar_left_repeat" && x.type !== "bar_right_repeat")) {
+K: perc stafflines = 1
+${measures.map((m) => m.map((x) => x.notation).join(" ")).join("|")}
+w:${measures.map((m) => m.map((x, index) => x.text.replace("#", index + 1)).join(" ")).join("|")}
+`;
+  let staff = ABCJS.renderAbc(staffDiv, abcString, { scale, staffwidth, paddingLeft: 0, paddingRight: 0 });
+  let beats = [];
+  let tripletMultiplier = 1;
+  let count = 0;
+  let beat = [];
+  let staffNotes = staff[0].lines[0].staff[0].voices[0].filter((x) => x.type !== "bar_left_repeat" && x.type !== "bar_right_repeat");
+  for (let note of staffNotes) {
     if (note.tripletMultiplier) {
-      tripletMultiplier = note.tripletMultiplier
+      tripletMultiplier = note.tripletMultiplier;
     }
-    count = count + (note.duration * tripletMultiplier)
-    beat.push(note)
+    count = count + note.duration * tripletMultiplier;
+    beat.push(note);
     if (count == 0.25) {
-      beats.push(beat)
-      count = 0
-      beat = []
+      beats.push(beat);
+      count = 0;
+      beat = [];
     }
     if (note.endTriplet) {
-      tripletMultiplier = 1
+      tripletMultiplier = 1;
     }
   }
-  el.appendChild(staffDiv)
+  el.appendChild(staffDiv);
 
   let question = createQuestion({
     light: true,
     questionText: t("pianoRythm"),
     answerNode: el,
-  })
-  question.querySelector(".answer").style.paddingLeft = 0
-  question.querySelector(".answer").style.paddingRight = 0
-  question.querySelector(".answer").click()
+  });
+  question.querySelector(".answer").style.paddingLeft = 0;
+  question.querySelector(".answer").style.paddingRight = 0;
+  question.querySelector(".answer").click();
 
-  const metronomeEl = h4()
-  updateMetronomeText(metronomeEl)
-  metronomeEl.classList.add("soundSetting")
-  metronomeEl.classList.add("light")
+  const metronomeEl = h4();
+  updateMetronomeText(metronomeEl);
+  metronomeEl.classList.add("soundSetting");
+  metronomeEl.classList.add("light");
   metronomeEl.addEventListener("click", (e) => {
-    e.stopPropagation()
+    e.stopPropagation();
     if (metronomePlaying) {
-      stopMetronome()
+      stopMetronome();
     } else {
       startMetronome((beat) => {
         for (let division of beats[(beat + 3) % 4]) {
           for (let elem of division.abselem.elemset) {
-            elem.style.fill = "currentColor"
+            elem.style.fill = "currentColor";
           }
         }
         for (let division of beats[beat]) {
           for (let elem of division.abselem.elemset) {
-            elem.style.fill = "#c90000"
+            elem.style.fill = "#c90000";
           }
         }
-      })
+      });
     }
-    updateMetronomeText(metronomeEl)
-  })
+    updateMetronomeText(metronomeEl);
+  });
 
-  addMetronomeControl(-10, question, metronomeEl)
-  addMetronomeControl(-1, question, metronomeEl)
-  question.appendChild(metronomeEl)
-  addMetronomeControl(1, question, metronomeEl)
-  addMetronomeControl(10, question, metronomeEl)
+  addMetronomeControl(-10, question, metronomeEl);
+  addMetronomeControl(-1, question, metronomeEl);
+  question.appendChild(metronomeEl);
+  addMetronomeControl(1, question, metronomeEl);
+  addMetronomeControl(10, question, metronomeEl);
 
+  const playTheBeat = h4(t("playTheBeat"));
+  playTheBeat.classList.add("soundSetting");
+  playTheBeat.classList.add("light");
+  playTheBeat.addEventListener("click", async (e) => {
+    e.stopPropagation();
+    stopMetronome();
+    if (!beatPlaying) {
+      beatPlaying = true;
+      playTheBeat.innerText = "Stop the beat next measure...";
+      await playBeats(staffNotes, playTheBeat);
+    } else {
+      playTheBeat.innerText = "Start the beat !";
+      beatPlaying = false;
+    }
+  });
+  question.appendChild(playTheBeat);
+}
+
+let beatPlaying = false;
+async function playBeats(notes) {
+  for (const staffNote of notes) {
+    if (!staffNote.rest) {
+      klack.pause();
+      klack.currentTime = 0;
+      klack.play();
+    }
+    await wait((60000 / metronomeTempo) * staffNote.duration * 4);
+  }
+  if (beatPlaying) {
+    await playBeats(notes);
+  }
+}
+
+function wait(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
 }
 
 function addMetronomeControl(delta, question, metronomeEl) {
-  const control = h4((delta > 0 ? "+" : "") + delta.toString())
-  control.classList.add("soundSetting")
-  control.classList.add("light")
-  control.addEventListener("click", () => { setMetronomeTempo(metronomeTempo + delta); updateMetronomeText(metronomeEl) })
-  question.appendChild(control)
+  const control = h4((delta > 0 ? "+" : "") + delta.toString());
+  control.classList.add("soundSetting");
+  control.classList.add("light");
+  control.addEventListener("click", () => {
+    setMetronomeTempo(metronomeTempo + delta);
+    updateMetronomeText(metronomeEl);
+  });
+  question.appendChild(control);
 }
 
 function updateMetronomeText(metronomeEl) {
   if (metronomePlaying) {
-    metronomeEl.innerText = `${t("metronome")} (${metronomeTempo}bpm): 🔊`
+    metronomeEl.innerText = `${t("metronome")} (${metronomeTempo}bpm): 🔊`;
   } else {
-    metronomeEl.innerText = `${t("metronome")} (${metronomeTempo}bpm): 🔈`
+    metronomeEl.innerText = `${t("metronome")} (${metronomeTempo}bpm): 🔈`;
   }
-
 }
--- js/strumming.js
+++ js/strumming.js
@@ -18,72 +18,72 @@ const patterns = [
   { name: "Pattern #18", pattern: "d d xuxu" },
   { name: "Pattern #19", pattern: "d d xudu" },
   { name: "Pattern #20", pattern: "d d  udu" },
-]
+];
 
 function formatStroke(stroke) {
   if (stroke === "d") {
-    return "↓"
+    return "↓";
   }
   if (stroke === "u") {
-    return "↑"
+    return "↑";
   }
   if (stroke === " ") {
-    return ""
+    return "";
   }
   if (stroke === "x") {
-    return "X"
+    return "X";
   }
 }
 function formatTempo(i, stroke) {
   if (stroke === " ") {
-    return ""
+    return "";
   }
   // if (stroke === "x") {
   //   return "X"
   // }
   if (i % 2 === 0) {
-    return (i / 2) + 1
+    return i / 2 + 1;
   }
-  return "et"
+  return "et";
 }
 function createPattern(pattern, chord) {
   // const patternEl = div("strumming")
   // let abcString = getAbcRythmNotation(pattern)
   // ABCJS.renderAbc(patternEl, abcString, { scale: 1.4, selectTypes: [], add_classes: true, staffwidth: 300 })
-  const patternEl = table("strumming")
-  patternEl.appendChild(caption(pattern.name + " en " + chord))
-  const split = pattern.pattern.split("")
-  const strokes = tr("strokes")
-  const tempo = tr("tempo")
+  const patternEl = table("strumming");
+  patternEl.appendChild(caption(pattern.name + " en " + chord));
+  const split = pattern.pattern.split("");
+  const strokes = tr("strokes");
+  const tempo = tr("tempo");
   for (let i = 0; i < split.length; i++) {
-    strokes.appendChild(td(formatStroke(split[i])))
-    tempo.appendChild(td(formatTempo(i, split[i])))
+    strokes.appendChild(td(formatStroke(split[i])));
+    tempo.appendChild(td(formatTempo(i, split[i])));
   }
-  patternEl.appendChild(strokes)
-  patternEl.appendChild(tempo)
-  return patternEl
+  patternEl.appendChild(strokes);
+  patternEl.appendChild(tempo);
+  return patternEl;
 }
 
 function getAbcRythmNotation({ name, pattern }) {
-  let countPerMeasure = 4
-  let key = "C"
-  let patternString = ""
-  let title = name
+  let countPerMeasure = 4;
+  let key = "C";
+  let patternString = "";
+  let title = name;
   for (let i = 0; i < pattern.length; i++) {
     if (pattern[i] == "d") {
-      patternString += "v!style=x!A"
+      patternString += "v!style=x!A";
     }
     if (pattern[i] == "u") {
-      patternString += "u!style=x!A"
+      patternString += "u!style=x!A";
     }
     if (pattern[i] == "x") {
-      patternString += "!style=x!A"
+      patternString += "!style=x!A";
     }
     if (pattern[i] == " ") {
-      patternString += "z"
+      patternString += "z";
     }
     if (i % 2) {
-      patternString += " "
+      patternString += " ";
     }
   }
   let abcString = `
@@ -93,187 +93,186 @@ M:${countPerMeasure}/4
 L:1/8
 K:${key}
 ${patternString}
-    `
+    `;
 
-  return abcString
+  return abcString;
 }
 
 function getRandomPattern() {
-  let pattern = ""
+  let pattern = "";
   for (let i = 0; i < 4; i++) {
-    pattern += chooseOne("xd ") + chooseOne("u ")
+    pattern += chooseOne("xd ") + chooseOne("u ");
   }
-  return { name: t("randomPattern"), pattern }
+  return { name: t("randomPattern"), pattern };
 }
 
 function addPatternToPartition(questionEl) {
-  const chord = getChord()
-  const partitionEl = questionEl.querySelector(".partition")
-  const pattern = Math.random() < 0.5 ? getRandomPattern() : chooseOne(patterns)
-  const patternEl = createPattern(pattern, chord)
-  partitionEl.appendChild(patternEl)
-  return { pattern, chord }
+  const chord = getChord();
+  const partitionEl = questionEl.querySelector(".partition");
+  const pattern = Math.random() < 0.5 ? getRandomPattern() : chooseOne(patterns);
+  const patternEl = createPattern(pattern, chord);
+  partitionEl.appendChild(patternEl);
+  return { pattern, chord };
 }
 
 function getChord() {
   if (settings.progressionChords == "practice") {
-    return chooseOne(possibleProgressionChords)
+    return chooseOne(possibleProgressionChords);
   }
-  return settings.progressionChords
+  return settings.progressionChords;
 }
 
-let sounds = []
+let sounds = [];
 
 function getSound(chord) {
-  const sound = sounds.find(x => x.chord === chord)
-  if(!sound) {
+  const sound = sounds.find((x) => x.chord === chord);
+  if (!sound) {
     let newSound = {
       chord: chord,
       up: new Audio(`assets/Acoustic/Acoustic-${chord}-Up.mp3`),
-      down: new Audio(`assets/Acoustic/Acoustic-${chord}-Down.mp3`)
-    }
-    sounds.push(newSound)
-    return newSound
+      down: new Audio(`assets/Acoustic/Acoustic-${chord}-Down.mp3`),
+    };
+    sounds.push(newSound);
+    return newSound;
   }
 
-  return sound
+  return sound;
 }
 
+const slap = new Audio("assets/Acoustic/slap.mp3");
+const pling = new Audio("assets/pling.mp3");
+const klack = new Audio("assets/klack.mp3");
+
 function strummingQuestion() {
-  const questionWrapper = div("question")
-  const partition = div("partition")
-  const tempo = t("tempo")(settings.tempo, settings.repeats, settings.progressionChords)
-  questionWrapper.appendChild(h4(tempo))
+  const questionWrapper = div("question");
+  const partition = div("partition");
+  const tempo = t("tempo")(settings.tempo, settings.repeats, settings.progressionChords);
+  questionWrapper.appendChild(h4(tempo));
 
-  questionWrapper.appendChild(partition)
-  let currentPattern = addPatternToPartition(questionWrapper)
-  let nextPattern = addPatternToPartition(questionWrapper)
-  const soundEl = h4(`${t("sound")}: 🔈`)
-  soundEl.classList.add("soundSetting")
-  questionWrapper.appendChild(soundEl)
-  let sound = false
+  questionWrapper.appendChild(partition);
+  let currentPattern = addPatternToPartition(questionWrapper);
+  let nextPattern = addPatternToPartition(questionWrapper);
+  const soundEl = h4(`${t("sound")}: 🔈`);
+  soundEl.classList.add("soundSetting");
+  questionWrapper.appendChild(soundEl);
+  let sound = false;
   soundEl.addEventListener("click", (e) => {
-    e.stopPropagation()
+    e.stopPropagation();
     if (sound) {
-      sound = false
-      soundEl.innerText = `${t("sound")}: 🔈`
+      sound = false;
+      soundEl.innerText = `${t("sound")}: 🔈`;
     } else {
-      sound = true
-      soundEl.innerText = `${t("sound")}: 🔊`
+      sound = true;
+      soundEl.innerText = `${t("sound")}: 🔊`;
     }
-  })
+  });
 
-  const metronomeEl = h4(`${t("metronome")}: 🔊`)
-  metronomeEl.classList.add("soundSetting")
-  let metronome = true
-  questionWrapper.appendChild(metronomeEl)
+  const metronomeEl = h4(`${t("metronome")}: 🔊`);
+  metronomeEl.classList.add("soundSetting");
+  let metronome = true;
+  questionWrapper.appendChild(metronomeEl);
   metronomeEl.addEventListener("click", (e) => {
-    e.stopPropagation()
+    e.stopPropagation();
     if (metronome) {
-      metronome = false
-      metronomeEl.innerText = `${t("metronome")}: 🔈`
+      metronome = false;
+      metronomeEl.innerText = `${t("metronome")}: 🔈`;
     } else {
-      metronome = true
-      metronomeEl.innerText = `${t("metronome")}: 🔊`
+      metronome = true;
+      metronomeEl.innerText = `${t("metronome")}: 🔊`;
     }
-  })
+  });
 
-  const instruction = p(t("clickToStart"))
-  questionWrapper.appendChild(instruction)
-  gameEl.prepend(questionWrapper)
-  let status = "pause"
-  let currentPartition = 0
-  let currentTempo = 0
-  let repeatTimes = 0
-  let timeoutId, activeStroke, activeTempo
-  const slap = new Audio("assets/Acoustic/slap.mp3")
-  const pling = new Audio("assets/pling.mp3")
-  const klack = new Audio("assets/klack.mp3")
+  const instruction = p(t("clickToStart"));
+  questionWrapper.appendChild(instruction);
+  gameEl.prepend(questionWrapper);
+  let status = "pause";
+  let currentPartition = 0;
+  let currentTempo = 0;
+  let repeatTimes = 0;
+  let timeoutId, activeStroke, activeTempo;
+
   questionWrapper.addEventListener("click", () => {
     if (status === "pause") {
       timeoutId = setInterval(async () => {
         if (activeStroke) {
-          activeStroke.classList.remove("active")
+          activeStroke.classList.remove("active");
         }
         if (activeTempo) {
-          activeTempo.classList.remove("active")
+          activeTempo.classList.remove("active");
         }
-        activeStroke = partition.querySelector(`.strumming:nth-child(${currentPartition + 1}) tr.strokes td:nth-child(${currentTempo + 1})`)
-        activeTempo = partition.querySelector(`.strumming:nth-child(${currentPartition + 1}) tr.tempo td:nth-of-type(${currentTempo + 1})`)
-        activeStroke.classList.add("active")
-        activeTempo.classList.add("active")
+        activeStroke = partition.querySelector(`.strumming:nth-child(${currentPartition + 1}) tr.strokes td:nth-child(${currentTempo + 1})`);
+        activeTempo = partition.querySelector(`.strumming:nth-child(${currentPartition + 1}) tr.tempo td:nth-of-type(${currentTempo + 1})`);
+        activeStroke.classList.add("active");
+        activeTempo.classList.add("active");
 
         if (metronome && currentTempo % 2 === 0) {
-          klack.pause()
-          klack.currentTime = 0
-          klack.play()
+          klack.pause();
+          klack.currentTime = 0;
+          klack.play();
         }
         if (metronome && currentTempo === 0) {
-          pling.pause()
-          pling.currentTime = 0
-          pling.play()
+          pling.pause();
+          pling.currentTime = 0;
+          pling.play();
         }
 
         if (sound) {
-
           if (settings.plingAtFirstTempo && currentTempo === 0) {
-            pling.pause()
-            pling.currentTime = 0
-            pling.play()
+            pling.pause();
+            pling.currentTime = 0;
+            pling.play();
           }
 
-
-
           if (currentPattern.pattern.pattern[currentTempo] === "d") {
-            const down = getSound(currentPattern.chord).down
-            down.pause()
-            down.currentTime = 0
-            down.play()
+            const down = getSound(currentPattern.chord).down;
+            down.pause();
+            down.currentTime = 0;
+            down.play();
           }
 
           if (currentPattern.pattern.pattern[currentTempo] === "u") {
-            const up = getSound(currentPattern.chord).up
-            up.pause()
-            up.currentTime = 0
-            up.play()
+            const up = getSound(currentPattern.chord).up;
+            up.pause();
+            up.currentTime = 0;
+            up.play();
           }
 
           if (currentPattern.pattern.pattern[currentTempo] === "x") {
-            slap.pause()
-            slap.currentTime = 0
-            slap.play()
+            slap.pause();
+            slap.currentTime = 0;
+            slap.play();
           }
         }
 
-        currentTempo = (currentTempo + 1) % 8
-        repeatTimes++
+        currentTempo = (currentTempo + 1) % 8;
+        repeatTimes++;
 
         if (repeatTimes >= parseInt(settings.repeats) * 8) {
-          currentPattern = { ...nextPattern }
-          nextPattern = addPatternToPartition(questionWrapper)
-          repeatTimes = 0
-          currentPartition++
-          partition.style.transform = `translateX(${-337 * currentPartition}px)`
+          currentPattern = { ...nextPattern };
+          nextPattern = addPatternToPartition(questionWrapper);
+          repeatTimes = 0;
+          currentPartition++;
+          partition.style.transform = `translateX(${-337 * currentPartition}px)`;
         } else {
         }
-      }, 30000 / parseFloat(settings.tempo))
-      instruction.innerText = t("clickToPause")
-      status = "play"
+      }, 30000 / parseFloat(settings.tempo));
+      instruction.innerText = t("clickToPause");
+      status = "play";
     } else {
-      clearInterval(timeoutId)
-      instruction.innerText = t("clickToStart")
-      status = "pause"
+      clearInterval(timeoutId);
+      instruction.innerText = t("clickToStart");
+      status = "pause";
     }
-  })
+  });
 }
 
 function printAllStrumming() {
-  const patternsEl = document.getElementById("patterns")
-  patternsEl.innerHTML = ""
+  const patternsEl = document.getElementById("patterns");
+  patternsEl.innerHTML = "";
   for (let patternIndex = 0; patternIndex < patterns.length; patternIndex++) {
-    let patternEl = createPattern(patterns[patternIndex], printNote("C"))
-    patternsEl.appendChild(patternEl)
+    let patternEl = createPattern(patterns[patternIndex], printNote("C"));
+    patternsEl.appendChild(patternEl);
   }
 }
 
-printAllStrumming()
+printAllStrumming();
